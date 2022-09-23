9183485e336ba14975937842e43609267f1d2bdc
0Venuxy <114260516+0Venuxy@users.noreply.github.com>
2022-09-23 17:38:47 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,42 @@
-
+let P2 = 0
+let P1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("Decolar!")
+    basic.pause(randint(1000, 5000))
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    P2 += 1
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    P1 += 1
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
